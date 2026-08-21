db8fde29defff253edb54507d9800edbe32df465
leetnoob <hiafi3@gmail.com>
2026-08-21 02:18:50 -0700

    Adding 10x quest and dungeon boss rewards

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,11 @@
 
 // This is where scripts' loading functions should be declared:
 // void MyExampleScript()
+void AddSC_custom_xp_rates();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
-    // MyExampleScript()
+    AddSC_custom_xp_rates();
 }
